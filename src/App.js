e1e19445148e7ca8b5675f45383a13ba1b3e140d
Loup <loup.peluso@gmail.com>
2021-02-11 12:03:44 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column"
+        }}
+      >
+        <div style={{ fontSize: "4rem" }}>
+          Pas encore!
+        </div>
+        <div style={{ fontSize: "2rem" }}>
+          C'est dans XXX.
+        </div>
+      </div>
     </div>
   );
 }
