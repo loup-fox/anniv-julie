d7027a677d7f2fca59a0444e185c8688ae0667b1
Loup <loup.peluso@gmail.com>
2021-02-11 12:40:54 +0100

    RC1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import logo from "./logo.svg";
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
+import { intervalToDuration } from "date-fns";
 
 function App() {
+  const [diff, setDiff] = useState(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      setDiff(
+        intervalToDuration({ end: new Date(2021, 2, 27), start: new Date() })
+      );
+    }, 100);
+  });
+
   return (
     <div className="App">
       <div
@@ -11,15 +23,20 @@ function App() {
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
-          flexDirection: "column"
+          flexDirection: "column",
         }}
       >
-        <div style={{ fontSize: "4rem" }}>
-          Pas encore!
-        </div>
-        <div style={{ fontSize: "2rem" }}>
-          C'est dans XXX.
-        </div>
+        <div style={{ fontSize: "4rem" }}>Pas encore!</div>
+        {diff && (
+          <>
+            <div style={{ fontSize: "2rem" }}>
+              C'est dans {diff.days} jours et {diff.hours} heures
+            </div>
+            <div style={{ fontSize: "1.5rem" }}>
+              {diff.minutes} minutes et {diff.seconds} secondes.
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
